10df25cbd2259ab7a082e92cdea849bb56fbb210
BrunoCalassi <bruno.calassi@gmail.com>
2022-11-01 23:53:25 -0300

    2

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,6 +1,8 @@
-import Fastify from "fastify"; //ligação entre back e front
-import { PrismaClient } from "@prisma/client"; //prisma conexão com banco de dados
-
+import Fastify from "fastify"; //ligação entre back e front npm i fastify
+import { PrismaClient } from "@prisma/client"; //prisma conexão com banco de dados npm i @prisma/client
+// npx prisma migrate dev
+// npx prisma studio  tipo django admin ve o bd
+import  cors  from "@fastify/cors";
 const prisma = new PrismaClient({
   log: ["query"],
 });
@@ -9,17 +11,15 @@ async function bootstrap() {
     logger: true,
   });
 
-  fastify.get("/pools/count", async () => {
-    const pools = await prisma.pool.findMany({
-      where: {
-        code: {
-          startsWith: "D",
-        },
-      },
-    });
+  await fastify.register(cors,{
+    origin: true,
+  })
 
-    return { pools };
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count()
+
+    return { count };
   });
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333,host:'0.0.0.0' });
 }
 bootstrap();
